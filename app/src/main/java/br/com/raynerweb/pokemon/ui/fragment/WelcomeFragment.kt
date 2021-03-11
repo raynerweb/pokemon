c400ffb9b399d7f82a772e25b54ac0fbb19137e0
Ráyner Carvalho Lima <raynerweb@gmail.com>
2021-03-11 10:38:09 -0300

    add navigation to next screen

--- app/src/main/java/br/com/raynerweb/pokemon/ui/fragment/WelcomeFragment.kt
+++ app/src/main/java/br/com/raynerweb/pokemon/ui/fragment/WelcomeFragment.kt
@@ -5,15 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import br.com.raynerweb.pokemon.R
+import br.com.raynerweb.pokemon.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        binding.fragment = this
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+
+    fun next(view: View) {
+        findNavController().navigate(R.id.action_welcomeFragment_to_usernameFragment)
     }
 
 }
