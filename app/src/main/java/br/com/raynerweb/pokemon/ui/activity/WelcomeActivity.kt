b51b8bab10d7997f4734a5933c2347e9eccdacd4
Ráyner Carvalho Lima <raynerweb@gmail.com>
2021-03-11 09:18:47 -0300

    add navigation support and create first fragments

--- app/src/main/java/br/com/raynerweb/pokemon/ui/activity/WelcomeActivity.kt
+++ app/src/main/java/br/com/raynerweb/pokemon/ui/activity/WelcomeActivity.kt
@@ -1,12 +1,12 @@
-package br.com.raynerweb.pokemon.ui
+package br.com.raynerweb.pokemon.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.raynerweb.pokemon.R
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
     }
 }
---
+++ app/src/main/java/br/com/raynerweb/pokemon/ui/fragment/PokemonTypeFragment.kt
@@ -0,0 +1,20 @@
+package br.com.raynerweb.pokemon.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.raynerweb.pokemon.R
+
+class PokemonTypeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pokemon_type, container, false)
+    }
+
+}
