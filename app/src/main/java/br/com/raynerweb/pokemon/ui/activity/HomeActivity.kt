6d1ddcc24509de4a36593d149fe85a492c88fac4
Ráyner Carvalho Lima <raynerweb@gmail.com>
2021-03-12 13:05:56 -0300

    home_activity

---
+++ app/src/main/java/br/com/raynerweb/pokemon/ui/activity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package br.com.raynerweb.pokemon.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.raynerweb.pokemon.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/br/com/raynerweb/pokemon/ui/fragment/PokemonTypeFragment.kt
+++ app/src/main/java/br/com/raynerweb/pokemon/ui/fragment/PokemonTypeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.raynerweb.pokemon.R
 import br.com.raynerweb.pokemon.databinding.FragmentPokemonTypeBinding
 
@@ -24,13 +26,12 @@ class PokemonTypeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.tvHello.text = getString(R.string.hello_trainer, "Ráyner")
 
     }
 
     fun next(view: View) {
-
+        findNavController().navigate(R.id.action_pokemonTypeFragment_to_homeActivity)
     }
 
 }
